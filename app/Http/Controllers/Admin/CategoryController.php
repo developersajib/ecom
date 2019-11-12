bdd28854c95e9eb772582d70d7bc266c168a884d
Sajib Hossain <sajibwebdeveloper@gmail.com>
2019-11-12 19:36:26 +0600

    Category add Part 1

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\BaseController;
+
+class CategoryController extends BaseController
+{
+    //
+}
